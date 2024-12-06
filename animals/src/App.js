f715bee6142ddf1cc2036fca5b7651864acf5345
Matthew McAlarney <memcalarney@wpi.edu>
2024-12-06 17:25:03 -0500

    Add count state variable to App.js

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,16 @@
+import { useState } from 'react'
+
 function App() {
+    const [count, setCount] = useState(0);
     const handleClick = () => {
+        setCount(count + 1);
         console.log("Button was clicked");
     };
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
+            <div>Number of animals: {count}</div>
         </div>
     );
 }
