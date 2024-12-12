738dacd90183d9c49d8eaf9158345ecb0e339580
Matthew McAlarney <memcalarney@wpi.edu>
2024-12-12 17:11:49 -0500

    Add increase heart width on click functionality

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,6 +1,35 @@
+import { useState } from 'react';
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    heart,
+    horse
+};    
+
 function AnimalShow({ type }) {
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    };
+
     return (
-        <div>{ type }</div>
+        // svgMap[type] access is used to look up a matching animal type key in the svgMap, and the matching key references one of the animal images that is imported above in the series of import statements.
+        <div onClick={handleClick}>
+            <img alt="animal" src={svgMap[type]} />
+            <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks }} />
+        </div>
     );
 }
 
