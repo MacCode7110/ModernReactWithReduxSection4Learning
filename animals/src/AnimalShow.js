1e3c6ab964370aaebca49659a602854c575483fd
Matthew McAlarney <memcalarney@wpi.edu>
2024-12-13 17:47:23 -0500

    Add App.css and AnimalShow.css files and style app

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,3 +1,5 @@
+//Import CSS files above other React hooks and components.
+import './AnimalShow.css';
 import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
@@ -26,9 +28,9 @@ function AnimalShow({ type }) {
 
     return (
         // svgMap[type] access is used to look up a matching animal type key in the svgMap, and the matching key references one of the animal images that is imported above in the series of import statements.
-        <div onClick={handleClick}>
-            <img alt="animal" src={svgMap[type]} />
-            <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks }} />
+        <div className="animal-show" onClick={handleClick}>
+            <img className="animal" alt="animal" src={svgMap[type]} />
+            <img className="heart" alt="heart" src={heart} style={{ width: 10 + 10 * clicks }} />
         </div>
     );
 }
